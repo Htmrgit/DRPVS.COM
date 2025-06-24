7ebb1c9fbf17d16d9bbf6b49285086f4d8093b51
MANOHAR KUMAR RAY (MANO) <mano.applicationdr@gmail.com>
2025-06-24 16:50:03 +0530

    Create userdata.js

---
+++ userdata.js
@@ -0,0 +1,35 @@
+// userdata.js
+
+// Static user database
+const users = [
+  {
+    name: "Ravi Kumar",
+    email: "manohar.deepanshu@gmail.com",
+    passcode: "Admin@01",
+    role: "Admin"
+  },
+  {
+    name: "Anjali Sharma",
+    email: "anjali@example.com",
+    passcode: "5678",
+    role: "Editor"
+  },
+  {
+    name: "Amit Singh",
+    email: "amit@example.com",
+    passcode: "0000",
+    role: "Viewer"
+  },
+  {
+    name: "Priya Jain",
+    email: "priya@example.com",
+    passcode: "9999",
+    role: "Support"
+  },
+  {
+    name: "Rajesh Mehta",
+    email: "rajesh@example.com",
+    passcode: "4444",
+    role: "User"
+  }
+];
